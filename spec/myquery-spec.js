2bbb0764e74d0f3c7e2c0baa87d9ba98eca99bcd
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-11 11:59:06 -0500

    forgot to commit changes, will go back and refactor

--- spec/myquery-spec.js
+++ spec/myquery-spec.js
@@ -16,19 +16,25 @@ describe("myQuery", function () {
       '</div>'
     );
   });
-
+  // when you call version, on the window, or assign it a property name
+  // you will get beta on it.
   it("has a version of value 'beta'", function() {
     expect($.version).toEqual('beta');
   });
-
+  // on the window there is an each function
+  // the each prop name is a function
+  // it takes two takes two parameters
+  // an array, and a function that takes a parameter
+  // the each function will push into an empty array
+  //
   describe("General each function", function () {
-    xit("iterates through an array", function () {
+    it("iterates through an array", function () {
       var testResult = [];
       var someArray = [10, 20, 30];
       $.each(someArray, function (number) {
-        testResult.push(number * number);
-      });
-
+        testResult.push(number * number);  // is this the function(number)
+      });                                  // being called here?
+      // push is the function(number)?
       expect(testResult.length).toEqual(3);
       expect(testResult[0]).toEqual(100);
       expect(testResult[1]).toEqual(400);
@@ -37,22 +43,30 @@ describe("myQuery", function () {
   });
 
   describe("Selectors", function () {
+       // $ = myQuery function
+       // pass in '#profile'
+       // get method on it, but remmber that
+       //myQuery takes in a parameter and returns an object
+       // a Querywrapper object
+       // get is a method on an object
+       // get is a property name of the myQuerry wrapper
 
-    xit("selects an element by id", function() {
-      var elem = $('#profile').get(0);
-      expect(elem.className).toEqual('noice');
-    });
+    it("selects an element by id", function() {
+      var elem = $('#profile').get(0); // get is a prop name of QueryWrapper object
+      expect(elem.className).toEqual('noice'); // retreive's classname from that html selector
+    });                                        // classname is an attribute for html elements
 
-    xit("selects elements by class name", function() {
-      var buttons = $('.button');
+    it("selects elements by class name", function() {
+      var buttons = $('.button'); // this will take in html, wrap around an object
+      expect(buttons.length).toEqual (2) // since it's an object, need to make length proper name point to value
       expect(buttons.get(0).className).toMatch(/first/);
       expect(buttons.get(1).className).toMatch(/second/);
     });
 
-    xit("selects elements by tag name", function() {
-      var anchors = $('a');
-      expect(anchors.length).toEqual(2)
-      expect(anchors.get(0).className).toEqual("button second");
+    it("selects elements by tag name", function() {
+      var anchors = $('a'); // take in html tag, in myQuery, turn to Querywrapper object
+      expect(anchors.length).toEqual(2) // length on Querywrapper object
+      expect(anchors.get(0).className).toEqual("button second"); // get(0) from an array, return html, className is
       expect(anchors.get(1).className).toEqual("straggler");
 
       var images = $('img');
@@ -62,13 +76,20 @@ describe("myQuery", function () {
   });
 
   describe("Selected elements each function", function () {
-    xit("iterates through all selected elements", function() {
-      var testResult = [];
-      $('.button').each(function (elem, i) {
-        testResult.push(elem.className + ' ' + i);
+    it("iterates through all selected elements", function() {
+      var testResult = []; // empty array
+      $('.button').each( function (elem, i) {  // one function that is anon, two parameters
+        testResult.push(elem.className + ' ' + i); //anon function called here
       });
 
-      expect(testResult.length).toEqual(2);
+      // BELOW IS THE SAME THING, but not an anon func
+      // var pushFunc = function (elem, i) {  // one function that is anon, two parameters
+      //   testResult.push(elem.className + ' ' + i); //anon function called here
+      // }
+      // $('.button').each(pushFunc); // pushfunc needs two parameters
+      // another element could have gone on line 70; after
+      // curly brakcets
+      expect(testResult.length).toEqual(2);       // length 2
       expect(testResult[0]).toEqual("button first 0");
       expect(testResult[1]).toEqual("button second 1");
     });
@@ -76,28 +97,60 @@ describe("myQuery", function () {
 
   describe("Show and Hide", function () {
     // TODO: Write tests for .show() and .hide()
+    it("hides the selected css element", function() {
+      var hiddenButton = $('.button').hide(); // call hide for button
+      expect(hiddenButton.get(0).style.display).toEqual('none') // expect display to be none
+   });
+    it("shows the selected css element", function() {
+      $('.button').hide(); // first need to hide button, then
+      var showButton = $('.button').show(); // call show on it, becaues hide is there
+      expect($('.button').get(0).style.display).toEqual('block'); // expect block to be there
+    });
   });
 
-  describe("addClass", function () {
+ describe("addClass", function () {
     // TODO: Write tests for addClass
     // HINT: Test using .toMatch() like the selector test
+    it("adds a class to an element", function() {
+      $('.button').addClass('poop'); // addClass is propname we make out
+      expect($('.button').get(0).className).toMatch(/poop/); // toMatch is regex
+    });  // addClass takes a parametr, the class to add.
+        // toMatch is regular expression if class includes poop
+        //
+
+    it("does not allow two classes of the same name on an element", function () {
+      $('.button').addClass('donkey');
+      $('.button').addClass('donkey');
+      expect($('.button').get(0).className).not.toMatch(/donkey donkey/);
+    })
   });
 
-  describe("Modifying CSS", function () {
 
-    xit("can set a single property", function() {
+ describe("Modifying CSS", function () {
+
+    it("can set a single property", function() {
       // Ensure they're not already hidden
       expect( $('.button').get(0).style.display ).toEqual('');
       expect( $('.button').get(1).style.display ).toEqual('');
 
       // Now make sure displays have updated
+      // you know you need to make a prop name called css
+      // you know that it takes two parameters
       $('.button').css('display', 'none');
       expect( $('.button').get(0).style.display ).toEqual('none');
       expect( $('.button').get(1).style.display ).toEqual('none');
     });
 
     // TODO: (`it` without a function are pending tests)
-    it("can set multiple properties in one call");
+    it("can set multiple properties in one call", function() {
+      $('.button').css({"border": "4px solid orange", "height": "200px"});
+
+      expect( $('.button').get(0).style.border ).toEqual("4px solid orange");
+      expect( $('.button').get(0).style.height ).toEqual("200px");
+
+      expect( $('.button').get(1).style.border ).toEqual("4px solid orange");
+      expect( $('.button').get(1).style.height ).toEqual("200px");
+    });
   });
 
 });
--- src/myquery.js
+++ src/myquery.js
@@ -2,12 +2,146 @@
 
   var QueryWrapper = function (elems) {
     // TODO
+
+    this.get = function(index) { // takes in an num for index
+      return elems[index]; // will take object wrapped around the
+    };   // the above will return an html element?
+
+    this.length = elems.length;
+
+    this.each = function(func) {
+      for (var i = 0; i < elems.length; i++ ) {
+        func(elems[i],i)
+      }
+      // $.each(elems, func);
+      return this; // return object
+    };
+
+    this.hide = function() {
+      // var css = elems.slice(1)
+      // var html = document.getElementsByClassName(css)
+      // html.style.display = 'none'
+
+      for(var i = 0; i < elems.length; i++) {
+        elems[i].style.display = "none";
+      }
+      //return elems.get(0); // why does't this work?
+      return this;
+    };
+
+    this.show = function() {
+      for(var i = 0; i< elems.length; i++ ) {
+        elems[i].style.display = 'block';
+      }
+      return this; //return elems.get(0); // why does't this work?
+    }
+
+    // this.addClass = function(className) {
+    //   for (var i = 0; i< elems.length; i++) {
+    //     if(elems.className) {
+    //       if(!elems.className.match(className)) {
+    //         elems.className += " " + className;
+    //       }
+    //     } else {
+    //       elems.className = className;
+    //     }
+    //   }
+    //   return this;
+    // }
+
+    this.addClass = function(className) {
+               // each takes in a function, with elem, what is elem?
+
+      this.each(function(elem) {  //saying if elem has a class?
+        if (elem.className) { // won't this always be true
+          if ( !elem.className.match(className) ) { // if elem doesn't match the class to be added
+            elem.className += " " + className; // add class name
+          }
+        } else {   // else, if there is no class to the element, add a class to it
+          elem.className = className;
+        }
+      });
+      return this;
+    };
+
+    // this.addClass = function(className) {
+    //         // this is anon function
+    //   this.each(function(elem) {
+    //         // checks if elem has a class
+    //     if (elem.className)) { // if className of element doesn't match class name to be added run block
+    //       if (!elem.className.match(className)) {
+    //         elem.className +=
+    //       } // if element doesn't have a class that is to be added
+
+    //     }
+    //   });
+
+    // };
+                           //display         // none
+      this.css = function(propertyOrObject, value) {
+      if (typeof propertyOrObject == "object") { // checking if css can select one or many properties
+                                                // if it is many then it will be an object, thus run block;
+        for (var style in propertyOrObject) { // loop through propnames/keys in object(many here)
+          this.each(function(elem) {   // for is looping through the key, values in object, each is looping through your html elements
+                                       // therefore you need too loops
+            elem.style[style] = propertyOrObject[style]; // each elem.style[style] can't do .style, will give .style as a string
+          });               // $('.button').css({"border": "4px solid orange", "height": "200px"});
+        }                   // object is {"border": "4px solid orange", "height": "200px"}
+                            // so, style is border, height. property)rObject[style] is 4px solid sorange, 200px in each loop
+      } else {              // so each   "div".style.border = 4px solid orange, then the next html in the array
+        this.each(function(elem) {       // if it's only one property, ie. ('display','none') in function(propertyOrObject, value)
+        elem.style[propertyOrObject] = value; // proeprty0rObject will equal display, value will be none
+        });                                   // you need to do [propertyorObject] to acces the variable, .propetyorObject will be a string
+      }
+      return this;
+    };
+
+
+
   };
 
   var myQuery = function (selector) {
     // TODO
+
+    if (selector.indexOf("#") === 0) { // check if parameter has a hash, at index 0
+      var newselector = selector.slice(1);
+      // debugger;
+      var element = document.getElementById(newselector);
+      var array = [];
+      array.push(element);
+      return new QueryWrapper(array);
+    } else if (selector.indexOf(".") === 0) {
+      // debugger;
+      var newselector = selector.slice(1);
+      var element = document.getElementsByClassName(newselector);
+      // var array = []; // ALREADY AN ARRAY!
+      // array.push(element); // RUN TEST WITH NODE ON, TO SEE!
+      console.log(element)
+      return new QueryWrapper(element);
+
+    } else {
+
+      var element = document.getElementsByTagName(selector);
+      return new QueryWrapper(element);
+    }
+
+
+
+
+
   };
 
   window.$ = myQuery;
+
+  $.each = function(array, func) {
+    for (var i = 0; i <array.length; i++) {
+      func(array[i],i);
+
+    }
+
+  };
+
+  $.version = 'beta'
+
 
 })();
